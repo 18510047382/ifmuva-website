c45ac304a9dbaaa5c1ead6d9b9293d2d3bac5baa
董英轩 <dongyingxuan@dongyingxuandeMacBook-Pro.local>
2019-12-16 17:16:56 +0800

    index update

--- js/main.js
+++ js/main.js
@@ -10,11 +10,19 @@
     xmlhttp.open("GET", "template/nav.html", true);
     xmlhttp.send();
 
-    new Vue({
-        el: '#about'
-    })
+    var xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.onreadystatechange = function() {
+        if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
+            document.querySelector("footer").innerHTML = xmlhttp2.responseText;
+            new Vue({
+                el: '#footer-join-us'
+            })
+        }
+    }
+    xmlhttp2.open("GET", "template/footer.html", true);
+    xmlhttp2.send();
 
     new Vue({
-        el: '#footer-join-us'
+        el: '#about'
     })
 }());
