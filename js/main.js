d11441154decd02d77c36642e38f6fe454bc8c29
董英轩 <dongyingxuan@dongyingxuandeMacBook-Pro.local>
2019-12-16 18:17:12 +0800

    flight-tracker update

--- js/main.js
+++ js/main.js
@@ -15,7 +15,15 @@
         if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
             document.querySelector("footer").innerHTML = xmlhttp2.responseText;
             new Vue({
-                el: '#footer-join-us'
+                el: '#footer-join-us',
+                methods: {
+                    joinUs: function() {
+                        this.$Modal.info({
+                            title: 'Join Us（加入 IFMUVA）',
+                            content: '欢迎加入我们的官方QQ群！QQ群号：991489400'
+                        })
+                    }
+                }
             })
         }
     }
