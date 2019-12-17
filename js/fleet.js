c3b36471a27b9ffbc63df263b4a2e948fd2fed46
董英轩 <dongyingxuan@dongyingxuandeMacBook-Pro.local>
2019-12-17 09:33:18 +0800

    update

--- js/fleet.js
+++ js/fleet.js
@@ -11,16 +11,22 @@
                 {
                     title: 'Model',
                     key: 'mod'
+                },
+                {
+                    title: 'Publish Time',
+                    key: 'pub'
                 }
             ],
             fleet: [
                 {
                     man: 'Airbus',
-                    mod: 'A350-900'
+                    mod: 'A350-900',
+                    pub: '2019-12-10'
                 },
                 {
                     man: 'Boeing',
-                    mod: 'B777-200F'
+                    mod: 'B777-200F',
+                    pub: 'Unknown'
                 }
             ]
         }
--- js/routes.js
+++ js/routes.js
@@ -7,6 +7,7 @@
             var routesData = JSON.parse(xmlhttp.responseText);
 
             for (var i = 100; i < routesData.length + 100; i++) {
+                routesData[i - 100].mod = 'Airbus A350-900';
                 routesData[i - 100].num = 'MU' + i;
             }
 
@@ -18,12 +19,20 @@
                             key: 'num'
                         },
                         {
+                            title: 'Recommend Model',
+                            key: 'mod'
+                        },
+                        {
                             title: 'Departure Airport',
                             key: 'da'
                         },
                         {
                             title: 'Arrive Airport',
                             key: 'aa'
+                        },
+                        {
+                            title: 'Flight Time',
+                            key: 'time'
                         }
                     ],
                     routes: routesData
