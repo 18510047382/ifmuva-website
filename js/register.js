da6784a3665f01b6d2227e6bf6bec5ea86f7b83f
BuildTools <unconfigured@null.spigotmc.org>
2020-02-13 09:35:07 +0800

    bug fix

--- js/register.js
+++ js/register.js
@@ -14,7 +14,7 @@
 
     var lang = navigator.language || navigator.userLanguage;
     lang = lang.substr(0, 2);
-    if (lang == 'zh') {
+    if (lang != 'zh') {
         modal.modal = true;
     }
 
