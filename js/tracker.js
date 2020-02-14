97cb94a7acf75c0257e559ef36fb0ac4464442d2
BuildTools <unconfigured@null.spigotmc.org>
2020-02-14 08:29:30 +0800

    mf pri

--- js/tracker.js
+++ js/tracker.js
@@ -1,7 +1,7 @@
-var lang = navigator.language || navigator.userLanguage;
-lang = lang.substr(0, 2);
-if (lang == 'zh') {
+// var lang = navigator.language || navigator.userLanguage;
+// lang = lang.substr(0, 2);
+// if (lang == 'zh') {
     document.querySelector('iframe').src = 'https://www.map-flight.com#MUVA';
-} else {
-    document.querySelector('iframe').src = 'https://liveflightapp.com/';
-}
+// } else {
+//     document.querySelector('iframe').src = 'https://liveflightapp.com/';
+// }
