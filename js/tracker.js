957fa13f8d572be364f2918a5846632a0b8f2b0f
BuildTools <unconfigured@null.spigotmc.org>
2020-03-14 11:10:01 +0800

    u

--- js/tracker.js
+++ js/tracker.js
@@ -1,7 +1,7 @@
 // var lang = navigator.language || navigator.userLanguage;
 // lang = lang.substr(0, 2);
 // if (lang == 'zh') {
-    document.querySelector('iframe').src = 'http://www.map-flight.com?apikey=4ab76a43-0a07-4ece-9ec2-7e07af09827f';
+    document.querySelector('iframe').src = 'https://www.map-flight.com?apikey=4ab76a43-0a07-4ece-9ec2-7e07af09827f';
 // } else {
 //     document.querySelector('iframe').src = 'https://liveflightapp.com/';
 // }
